9775c2139a522aefc8c8c6e4cf7d94caf13cd476
Andre <andre.d.emmenegger@gamil.com>
2022-01-07 22:33:10 +0100

    works now

--- simplescript.py
+++ simplescript.py
@@ -12,21 +12,22 @@ class ffmpegHandler():
     def splitInput(self):
         #ffmpeg -i out.mkv -vn -c:a libmp3lame -y audio.mp3 besser glaub
         splitAudio = self.ffmpegPath + " -i " + self.input + " -vn -c:a libmp3lame -y out.mp3"
-        splitVideo = self.ffmpegPath + " -i " + self.input + " " + self.output + "img%04d.jpg"
+        splitVideo = self.ffmpegPath + " -i " + self.input + " " + self.output + "img%04d.png"
 
         os.system(splitAudio)
         os.system(splitVideo)
 
     def combineToVideo(self, frameLoc, out, frameRate):
-        combineInstr = self.ffmpegPath +" -r " + frameRate + " -start_number 0001 -f image2 -s 3568x1592 -i thumb%04d.png -vcodec libx264 -crf 25 -pix_fmt yuv420p upscaled.mp4"
+        combineInstr = self.ffmpegPath +" -r " + frameRate + " -start_number 0001 -f image2 -s 3840x2160" + " -i " + frameLoc + "/img%04d.png" + " -vcodec libx264 -crf 25 -pix_fmt yuv420p upscaled.mp4"
         os.system(combineInstr)
         
     def addSound(self):
-        mergeAudio = self.ffmpegPath + " -i " + "upscaled.mp4" + " -i " + "out.mp3" + " -c -copy map 0:v:0 -map 1:a:0 final.mp4"
+        mergeAudio = self.ffmpegPath + " -i " + "upscaled.mp4" + " -i " + "out.mp3" + " -c copy -map 0:v:0 -map 1:a:0 final.mp4"
         os.system(mergeAudio)
 
 def upscale(neuralNetType, toUpscale, upScaled):
-    upscaleInstr = "realesrgan-ncnn-vulkan.exe -i " + toUpscale + " -o "+ upScaled+ " -n " + neuralNetType + " -t 400"
+    #upscaleInstr = "realesrgan-ncnn-vulkan.exe -i " + toUpscale + " -o "+ upScaled+ " -n " + neuralNetType + " -t 400"
+    upscaleInstr = "realesrgan-ncnn-vulkan.exe -i " + toUpscale + " -o "+ upScaled+ " -n " + neuralNetType + " -s 2 -f png"
     os.system(upscaleInstr)
 
 
@@ -36,8 +37,18 @@ def main():
     input = "input/in.mkv"
     intermed = "./frames/"
     ffmpeg_path = "ffmpeg.exe"
-    neuralNet = "realesrgan-x4plus-anime"
+    neuralNet = "RealESRGANv2-animevideo-xsx2"
+    upscaleLoc = "./upscaled"
+    #upscaleLoc = "C:\\Users\\andre\\Videos\\superscale\\upscaled"
     ff = ffmpegHandler(input,intermed,ffmpeg_path)
+    
+    
+    #ff.splitInput()
+    upscale(neuralNet, "frames", upscaleLoc)
+    ff.combineToVideo(upscaleLoc, "upscaled.mp4", "23.976215")
+    ff.addSound()
+
+
     """video = cv2.VideoCapture(input)
     fps = video.get(cv2.CAP_PROP_FPS)
     ff.splitInput()
@@ -45,10 +56,6 @@ def main():
     ff.combineToVideo("./upscale", "upscaled.mp4", fps)
     ff.addSound()
     """
-    ff.splitInput()
-    upscale(neuralNet, "frames", "upscaled")
-    ff.combineToVideo("./upscale", "upscaled.mp4", "24")
-    ff.addSound()
     
 
     
